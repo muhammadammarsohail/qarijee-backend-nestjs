3d244eef2f6c58954e1881669cdaf4e390877e49
Ammar Sohail <muhammadammarsohail@gmail.com>
2022-02-21 00:10:32 +0500

    Developed: API: get course by name with teachers

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Delete, Get, Post, Put, Request } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Post, Put, Request } from "@nestjs/common";
+import { CourseEnum } from "src/enum/courseEnum";
 import { CourseService } from "./course.service";
 
 @Controller("course")
@@ -16,13 +17,23 @@ export class CourseController {
     return this.courseService.getCourseNames(token);
   }
 
-  @Get("all")
+  @Get('all')
   async getAllCourses(
     @Request() req: any
   ) {
     const authHeader = req.headers['authorization']
     const token = authHeader.split(' ')[1];
     return this.courseService.getAllCourses(token);
+  }
+
+  @Get('/:name')
+  async getCourseByName(
+    @Param('name') name: CourseEnum,
+    @Request() req: any
+  ) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader.split(' ')[1];
+    return this.courseService.getCourseByName(name, token);
   }
 
   @Post("/:id")
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -16,7 +16,16 @@ export class CourseService {
 
     async getAllCourses(token: string) {
         authenticate([Role.admin, Role.student, Role.teacher], token)
-
+        
         return db.course;
+    }
+
+    async getCourseByName(courseName: CourseEnum, token: string) {
+        authenticate([Role.admin, Role.student, Role.teacher], token)
+
+        let [course] = db.course.filter(course => course.name === courseName);
+        const teachers = db.teacher.filter(teacher => teacher.courses.include(courseName));
+        course['teachers'] = teachers;
+        return course;
     }
 }
